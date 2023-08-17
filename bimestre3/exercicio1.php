65485babd90eeb9aeee4a1e3370b76098aa5eec1
juan <juanpmad@outlook.com>
2023-08-17 11:56:23 -0300

    bimestre3

--- bimestre3/exercicio1.php
+++ bimestre3/exercicio1.php
@@ -4,29 +4,103 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>PHP</title>
+        <title>Exercício 1 - PHP</title>
+        <style>
+            /* cursor */
+            * {
+                cursor: url("https://ani.cursors-4u.net/games/gam-16/gam1561.cur"), auto;
+            }
+
+            /* cor ao selecionar texto */
+            ::selection {
+                text-shadow: 0 0 2px #00ff60;
+                color:#00ff60;
+            }
+            
+            ::-moz-selection {
+                text-shadow: 0 0 2px #00ff60;
+                color:#00ff60;
+            }
+
+            /* scrollbar */
+            ::-webkit-scrollbar {
+                width: 5px;
+                height: 10px;
+                background: #000000;
+            }
+            
+            ::-webkit-scrollbar-thumb {
+                background: #00abab50;  
+                border-radius: 10px;
+            }
+
+            ::-webkit-scrollbar-thumb:hover {
+                background: #008b8b50;  
+            }
+
+            /* body */
+            body {
+                font-family: "VT-100", fixedsys, System, monospace;
+                display: flex;
+                background-color: black;
+                color: #00ffff;
+            }
+
+            table {
+                text-align: center;
+                margin: 0 auto;
+                padding: 10px;
+                border: solid 2px #e1910566;
+                column-gap: 200px;
+                min-width: 25%;
+            }
+
+            table th {
+                padding: 0 25px;
+            }
+
+            table td {
+                margin: 0;
+                padding: 4px;
+            }
+
+            table tr:first-child {
+                text-decoration: none;
+                color: #e0ffff;
+            }
+
+            table tr:nth-child(odd) {
+                background: #1a1a1a90;
+            }
+        </style>
+        <!--
+        Criar uma tabela em HTML e PHP, que crie um indice a partir do 0 até 20, com as seguintes colunas: 
+        apresentar a potenciacao por 2
+        apresentar o fatorial.
+        exemplo:
+        0   0   1
+        1   1   1
+        2   4   2
+        3   9   6    
+        -->
     </head>
     <body>
         <table>
             <tr>
-                <th>Ferramenta</th>
-                <th>Preço</th>
+                <th>Número</th>
+                <th>Quadrado</th>
+                <th>Fatorial</th>      
             </tr>
             <?php
-                $dados = array (
-                    "chave_de_fenda" => 6.00,
-                    "parafuso" => 0.40,
-                    "alicate" => 12.00,
-                    "multimetro" => 31.50
+                $array = array(
+                    0 => array(),
+                    1 => array()
                 );
-                foreach ($dados as $key => $value) {
-                    echo "
-                    <tr>
-                        <td>$key</td>
-                        <td> $value</td>
-                    </tr>
-                    ";
-                }
+                for ($i=1; $i <= 20; $i++) { 
+                    $array[0][$i] = ($i**2);
+                    $array[1][$i] = array_product(range($i, 1));
+                    echo "<tr><td>".$i."</td><td>".$array[0][$i]."</td><td>".$array[1][$i]."</td></tr>";
+                }    
             ?>
         </table>
     </body>
--- bimestre3/trabalhoAssinantes.sql
+++ bimestre3/trabalhoAssinantes.sql
@@ -1,63 +1,69 @@
 -- cria tabela Assinante
 create table Assinante (
     cd_assinante serial primary key,
-    nm_assinante varchar(20) not null
+    nm_assinante varchar(100),
+    mun_assinante int not null,
+    ramo_assinante int not null,
+    tip_assinante int not null,
+    ende_assinante int not null
 );
 
-insert into Assinante (nm_assinante) values
-('Laura Palmer'),
-('Audrey Horne'),
-('Donna Hayward'),
-('Dale Cooper');
+insert into Assinante (nm_assinante, tip_assinante, mun_assinante, ramo_assinante, ende_assinante) values
+('Laura Palmer', 2, 5, 2, 1),
+('Dale Cooper', 1, 2, 5, 2);
 
--- cria tabela Ramo_Atividade
-create table Ramo_Atividade (
-    cd_ramo serial primary key,
-    ds_ramo varchar(90) not null
-);
-
-insert into Ramo_Atividade (ds_ramo) values
-('Descrição 1'),
-('Descrição 2'),
-('Descrição 3'),
-('Descrição 4');
-
--- cria tabela Tipo_Assinante
+-- cria a tabela Tipo_Assinante
 create table Tipo_Assinante (
     cd_tipo serial primary key,
-    ds_tipo varchar(90) not null
+    ds_tipo varchar(100) not null
 );
 
-insert into Tipo_Assinante (ds_tipo) values
+insert into tipo_assinante (ds_tipo) values
 ('Comercial'),
 ('Residencial');
 
--- cria tabela Municipio
+-- cria a tabela Ramo_Atividade
+create table Ramo_Atividade (
+    cd_ramo serial primary key,
+    ds_ramo varchar(100) not null
+);
+
+insert into Ramo_Atividade (ds_ramo) values
+('generic_ds_ramo1'),
+('generic_ds_ramo2'),
+('generic_ds_ramo3'),
+('generic_ds_ramo4'),
+('generic_ds_ramo5');
+
+-- cria a tabela Municipio
 create table Municipio (
     cd_municipio serial primary key,
     ds_municipio varchar(100) not null
 );
 
-insert into Municipio (ds_municipio) values
+insert into municipio (ds_municipio) values
+('generic_ds_municipio1'),
+('Porto Alegre'),
+('generic_ds_municipio3'),
+('generic_ds_municipio4'),
 ('Pelotas'),
-('Porto Alegre');
+('generic_ds_municipio6'),
+('generic_ds_municipio7'),
+('generic_ds_municipio8'),
+('generic_ds_municipio9'),
+('generic_ds_municipio0');
 
--- cria tabela Endereco
+-- cria a tabela Endereco
 create table Endereco (
     cd_endereco serial primary key,
     ds_endereco varchar(100) not null,
     complemento varchar(100),
     bairro varchar(100) not null,
-    cep int not null
+    cep int not null,
+    mun_ende int not null
 );
 
-insert into Endereco (ds_endereco, complemento, bairro, cep) values
-('Rua Falls Avenue', 'Restaurante Double R Diner', 'Main Street', '90880020'),
-('Rua Not Far', 'Taverna Roadhouse', 'Main Street', '93347320'),
-('Rua Great Northern Highway', 'Hotel Great Northern', 'White Tail Falls', '93420280'),
-('Rua Highway 21', 'Posto de Gasolina', 'Lower Town', '92110400');
-
--- cria tabela de Telefone
+-- cria tabela Telefone
 create table Telefone (
     cd_fone serial primary key,
     ddd int not null,
@@ -65,45 +71,31 @@ create table Telefone (
     assinante_fone int not null
 );
 
-insert into Telefone (ddd, n_fone, assinante_fone) values
-('53', '941598756', '1'),
-('53', '924679184', '2'),
-('53', '981427356', '3'),
-('53', '997846221', '4');
+-- popula a tabela de telefones
+insert into telefone (ddd, n_fone, assinante_fone) values
+(53, 941598756, 1),
+(54, 924679184, 1),
+(53, 981427356, 2);
+
+-- popula a tabela de endereços
+insert into endereco (ds_endereco, complemento, bairro, cep, mun_ende) values
+('Rua Falls Avenue', 'Restaurante Double R Diner', 'Main Street', '90880020', 5),
+('Rua Not Far', 'Taverna Roadhouse', 'Main Street', '93347320', 2);
 
 --listar os nomes dos assinantes, seguido dos dados do endereço e os telefones correspondentes.
-select nm_assinante as nome,
-e.ds_endereco as endereco,
-t.n_fone as contato from Assinante
-join endereco e on e.cd_endereco = cd_assinante
-join telefone t on t.assinante_fone = cd_assinante;
+select nm_assinante as nome, e.ds_endereco as endereco, t.n_fone as contato from assinante join endereco e on e.cd_endereco = ende_assinante join telefone t on t.assinante_fone = cd_assinante;
 
 --listar os nomes dos assinantes, seguido do seu ramo, ordenados por ramo e posteriormente por nome.
-select nm_assinante as nome,
-r.ds_ramo as ramo from Assinante
-join ramo_atividade r on r.cd_ramo = ramo_assinante
-order by ds_ramo, nm_assinante;
+select nm_assinante as nome, r.ds_ramo as ramo from assinante join ramo_atividade r on r.cd_ramo = ramo_assinante order by ds_ramo, nm_assinante;
 
 --listar os assinantes do município de pelotas que são do tipo residencial.
-select nm_assinante as nome from Assinante where Tipo_Assinante = (
-    select cd_tipo from tipo_assinante where ds_tipo = 'Residencial'
-)
-and ende_assinante = (
-    select cd_municipio from Municipio where ds_municipio = 'Pelotas'
-);
+select nm_assinante as nome from assinante where tip_assinante = (select cd_tipo from tipo_assinante where ds_tipo = 'residencial') and mun_assinante = (select cd_municipio from municipio where ds_municipio = 'pelotas');
 
 --listar os nomes dos assinantes que possuem mais de um telefone.
-select nm_assinante as nome from Assinante where (
-    select count(cd_assinante = (select cd_fone from telefone)) from Assinante
-) > 1;
+select nm_assinante as nome from assinante where (select cd_assinante = (select assinante_fone from telefone group by assinante_fone having count(assinante_fone)>1));
 
---listar os nomes dos assinantes seguido do número do telefone, tipo de assinante comercial, com endereço em Porto Alegre.
-select nm_assinante as nome,
-t.n_fone as contato from Assinante
-join telefone t on cd_assinante = t.assinante_fone where cd_assinante = (
-    select cd_tipo from Tipo_Assinante where ds_tipo = 'Comercial'
-)
-and cd_assinante = (select cd_municipio from municipio where ds_municipio = 'Porto Alegre');
+--listar os nomes dos assinantes seguido do número do telefone, tipo de assinante comercial, com endereço em porto alegre.
+select nm_assinante as nome, t.n_fone as contato from assinante join telefone t on cd_assinante = t.assinante_fone where tip_assinante = (select cd_tipo from tipo_assinante where ds_tipo = 'comercial') and mun_assinante = (select cd_municipio from municipio where ds_municipio = 'porto alegre');
 
 -- exclui as tabelas
-drop table Assinante, Ramo_Atividade, Tipo_Assinante, Municipio, Endereco, Telefone;
+drop table Municipio, Endereco, Ramo_Atividade, Tipo_Assinante, Telefone, Assinante;

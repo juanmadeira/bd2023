eac72cfc527601f1ae1ce54d817e9fb2abf8cf5b
juan <juanpmad@outlook.com>
2023-09-05 16:26:46 -0300

    bimestre3

--- bimestre3/exercicio2.php
+++ bimestre3/exercicio2.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercício 2 - PHP</title>
+    <title>exercício 2 - php</title>
     <style>
         /* cursor */
         * {

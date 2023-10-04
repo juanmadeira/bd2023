f19e4097c042804cdbdbf75237253a129ce01d4c
Juan <juanpmad@outlook.com>
2023-10-03 23:11:39 -0300

    bimestre3

--- bimestre3/trabalhoPhp/trabalhoPhp.sql
+++ bimestre3/trabalhoPhp/trabalhoPhp.sql
@@ -7,6 +7,6 @@ create table Itens (
 
 insert into Itens (nome_item, qtd_item, preco_item) values
 ('Contrabaixo Strinberg Precision', '2', '1299.99'),
-('Guitarra Epiphone SG', '4', '6499.99'),
+('Guitarra Epiphone SG', '0', '6499.99'),
 ('Guitarra Fender Jazzmaster', '7', '7499.99'),
 ('Guitarra Tagima MG30 Stratocaster', '16', '769.99');

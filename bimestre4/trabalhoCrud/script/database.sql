b0fefdfb59fe45a591c55c4388c062c2f0d8dd9e
juan <juan.madeira@aluno.riogrande.ifrs.edu.br>
2023-11-30 14:41:45 -0300

    script

--- bimestre4/trabalhoCrud/script/database.sql
+++ bimestre4/trabalhoCrud/script/database.sql
@@ -1,10 +1,10 @@
---cria o banco de dados do trabalho
+-- cria o banco de dados do trabalho
 CREATE DATABASE trabalho41;
 
---conecta no banco de dados
+-- conecta no banco de dados
 \c trabalho41
 
---cria a tabela dos usuários
+-- cria a tabela dos usuários
 CREATE TABLE usuario (
     id SERIAL PRIMARY KEY,
     nm VARCHAR(40) NOT NULL,
@@ -13,7 +13,7 @@ CREATE TABLE usuario (
     isAdm INT
 );
 
---cria a tabela dos livros
+-- cria a tabela dos livros
 CREATE TABLE livro (
     id SERIAL PRIMARY KEY,
     autores VARCHAR(100) NOT NULL,
@@ -24,7 +24,7 @@ CREATE TABLE livro (
     quantidade INT NOT NULL
 );
 
---cria a tabela da relação entre usuário e livro
+-- cria a tabela da relação entre usuário e livro
 CREATE TABLE emprestimo (
 	idEmprestimo SERIAL PRIMARY KEY,
 	tempo TIMESTAMP NOT NULL,
@@ -34,15 +34,15 @@ CREATE TABLE emprestimo (
 	FOREIGN KEY (idLivro) REFERENCES livro (id)
 );
 
---insere um usuário teste
+-- insere um usuário teste
     --email: nome@email.com
     --senha: senha
 INSERT INTO usuario (nm, email, senha) VALUES ('nome', 'nome@email.com', '$2y$10$U31TBJhtrPWah/MqKjORYeilUaaIQ4WzE58cWTdK4E9OE6ZOB2M6.');
 
---insere o usuário administrador
+-- insere o usuário administrador
     -- email: adm@email.com
     -- senha: adm
 INSERT INTO usuario (nm, email, senha, isadm) VALUES ('anya taylor joy', 'adm@email.com', '$2y$10$tbRtK9vUCQWmCFXiGEZLWOGD4SyVYia/vSV5Rn.3iJGQFakDU67/6', 1);
 
---insere um livro teste
+-- insere um livro teste
 INSERT INTO livro (autores, titulo, imagem, ano, editora, quantidade) VALUES ('Garrett Romines', 'A Biblia Para Minecrafters', 'uploads/a-biblia-para-minecrafters.jpg', '2016', 'BK BOOKS', '1');
